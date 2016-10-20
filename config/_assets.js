7f001a8e01b979957692fdbfe49a3407b04afdeb
dcunha <dcunha@medicushcs.com>
2016-10-20 11:48:04 -0400

    Init

---
+++ config/_assets.js
@@ -0,0 +1,6 @@
+// load jquery and foundation in the window scope
+import 'script!jquery';
+import 'script!what-input';
+import 'script!foundation-sites';
+
+import 'src/assets/scss/storefront.scss';
